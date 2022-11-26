3a06a93061cf41e08a4b1916897554de16a006ee
Adalbertus250 <killergames156@gmail.com>
2022-11-26 20:22:53 +0100

    🔨Implementace Turnstile Sites od Cloudflare (Nová alternativa k ReCaptcha & HCaptcha splňující GDPR a další nařízení)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <title inertia>PROJECT MUJIWA | Nová [Anime, Dorama, Manga] databáze založena na staré akihabaře a akidb</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900|poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
     </head>
     <body>
 
