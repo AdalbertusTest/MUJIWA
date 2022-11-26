03cca8577aed23ae0dd109a706b8531849d2c28c
Adalbertus250 <killergames156@gmail.com>
2022-11-26 20:16:14 +0100

    💄Implementace Fontu Inter pomocí Fonts.bunny.net (Alternativa k google fonts bez sledování splňující GDPR)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,8 @@
         <meta name="keywords" content="Anime, Manga, Dorama, Fansubs, Fansub, Subtitles, MUJIWA">
         <meta name="description" content="{{$meta['description'] ?? 'PROJECT MUJIWA - Pro ty, kteří věnují svůj čas asijské kultuře.'}}">
         <title inertia>PROJECT MUJIWA | Nová [Anime, Dorama, Manga] databáze založena na staré akihabaře a akidb</title>
-
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900|poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
     </head>
     <body>
 
