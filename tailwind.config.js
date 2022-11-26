4e0ad497d7be06b1e5fe205a060dbdcca9207fb6
Adalbertus250 <killergames156@gmail.com>
2022-11-26 21:40:07 +0100

    🔧Základní nastavení Tailwindu pro projekt
    Bylo zde nastaveno několik věcí, implementace fontu Inter který portuji pomocí fonts.bunny.net, přidání brand color MW, implementaci pluginu do tailwindu jako reset forms css od TW, typography systém od TW, ovládání css stavu pomocí headlessui od TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      './resources/client/**/*.{jsx,js,tsx,ts}',
+      './resources/views/**/*.blade.php',
+      './resources/views/vendor/**/*.blade.php'
+  ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            brand: '#0099FF'
+        },
+        fontFamily: {
+            inter: ['Inter', ...defaultTheme.fontFamily.sans]
+        }
+    },
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+      require('@headlessui/tailwindcss')
+  ],
 }
