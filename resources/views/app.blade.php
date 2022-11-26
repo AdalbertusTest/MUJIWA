2b3e9c8b5fffd1e7f3546676d492553639fdb5a5
Adalbertus250 <killergames156@gmail.com>
2022-11-26 20:07:47 +0100

    🔨Implementace základní HTML kostry

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="TEAM MUJIWA">
+        <meta name="keywords" content="Anime, Manga, Dorama, Fansubs, Fansub, Subtitles, MUJIWA">
+    </head>
+    <body>
+
+    </body>
+</html>
