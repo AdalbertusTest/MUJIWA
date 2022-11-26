11aec01d7b9fd5f7f1c6f16edafeea8283b9e993
Adalbertus250 <killergames156@gmail.com>
2022-11-26 20:13:12 +0100

    🔨Implementace základní HTML Tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="TEAM MUJIWA">
         <meta name="keywords" content="Anime, Manga, Dorama, Fansubs, Fansub, Subtitles, MUJIWA">
+        <meta name="description" content="{{$meta['description'] ?? 'PROJECT MUJIWA - Pro ty, kteří věnují svůj čas asijské kultuře.'}}">
+        <title inertia>PROJECT MUJIWA | Nová [Anime, Dorama, Manga] databáze založena na staré akihabaře a akidb</title>
+
     </head>
     <body>
 
